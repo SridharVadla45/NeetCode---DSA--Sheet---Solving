1b8b721d52b1488571b6e019a6192a3ce90dce65
SridharVadla <vadlasreedhar123@gmail.com>
2025-03-09 18:31:09 +0530

    problem 146 : word ladder

--- src/Graph/Problems/shortestpath/wordladder/Solution.java
+++ src/Graph/Problems/shortestpath/wordladder/Solution.java
@@ -1,31 +1,59 @@
-//package src.Graph.Problems.shortestpath.wordladder;
-//
-//import java.util.HashSet;
-//import java.util.LinkedList;
-//import java.util.List;
-//import java.util.Queue;
-//
-//public class Solution {
-//    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-//
-//        Queue<String> queue = new LinkedList<>();
-//        HashSet<String> words = new HashSet<>();
-//        for(String s :wordList){
-//            words.add(s);
-//        }
-//        queue.add(beginWord);
-//        if(words.contains(beginWord)) words.remove(beginWord);
-//
-//        while(!queue.isEmpty()){
-//            char [] current = queue.poll().toCharArray();
-//            int len = current.length;
-//            for(int i=0;i<len;i++){
-//                for(int j=0;j<26;j++){
-//                    current[i]= (char) ('a'+j);
-//                    if(current)
-//                }
-//            }
-//        }
-//
-//    }
-//}
+package src.Graph.Problems.shortestpath.wordladder;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.*;
+
+public class Solution {
+    static class Pair {
+        String val;
+        int level;
+
+        Pair(String val, int level) {
+            this.val = val;
+            this.level = level;
+        }
+    }
+
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Queue<Pair> queue = new LinkedList<>();
+        HashSet<String> words = new HashSet<>(wordList); // Directly add all words
+
+        if (!words.contains(endWord)) {
+            return 0; // If endWord is not in wordList, no transformation is possible.
+        }
+
+        queue.add(new Pair(beginWord, 1)); // Start level at 1
+        words.remove(beginWord); // Remove beginWord from the set
+
+        while (!queue.isEmpty()) {
+            Pair original = queue.poll();
+            String currentWord = original.val;
+            int currentLevel = original.level;
+
+            if (currentWord.equals(endWord)) {
+                return currentLevel;
+            }
+
+            char[] currentChars = currentWord.toCharArray();
+            int len = currentChars.length;
+
+            for (int i = 0; i < len; i++) {
+                char originalChar = currentChars[i]; // Store the original char
+                for (char j = 'a'; j <= 'z'; j++) {
+                    currentChars[i] = j;
+                    String newWord = new String(currentChars);
+
+                    if (words.contains(newWord)) {
+                        queue.add(new Pair(newWord, currentLevel + 1));
+                        words.remove(newWord);
+                    }
+                }
+                currentChars[i] = originalChar; // Restore the original char
+            }
+        }
+        return 0;
+    }
+}
